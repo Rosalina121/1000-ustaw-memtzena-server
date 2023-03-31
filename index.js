566def0c537471ffe553e76dc00b7922bab506cf
Alina Rosa <rosalina@palitechnika.com>
2023-03-31 20:59:13 +0200

    add new ustawy and new methods

--- index.js
+++ index.js
@@ -5,23 +5,37 @@ const cors = require('cors')
 const app = express()
 const port = 3000
 
-let ustawy;
-
-
+let ustawy = [];
+let availableUstawy = [];
 // console.log(ustawy)
+
+loadData()
 
 app.use(cors())
 
-
-
 app.get('/ustawa/:id', (req, res) => {
-    let data = fs.readFileSync('ustawy.json')
-    ustawy = JSON.parse(data).ustawy
     const ustawa = ustawy.find(v => v.number === req.params.id)
     console.log(ustawa)
     res.send(ustawa)
 })
 
+app.get('/ustawa', (req, res) => {
+    const ustawa = availableUstawy[availableUstawy.length * Math.random() | 0]
+    console.log("Ustawa get!", ustawa)
+    res.send(ustawa)
+})
+
+app.get('/available', (req, res) => {
+    res.send({availableUstawy: availableUstawy})
+})
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
+
+function loadData() {
+    let data = fs.readFileSync('ustawy.json')
+    ustawy = JSON.parse(data).ustawy
+    availableUstawy = ustawy.filter(u => u.title !== "")
+    console.log(availableUstawy)
+}
